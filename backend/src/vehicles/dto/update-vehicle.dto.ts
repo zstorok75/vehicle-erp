ddd3a4c690c43acbca9b7d45346f9c60d5f306da
zstorok75 <zstorok75@gmail.com>
2026-07-06 20:19:46 +0200

    refactor(backend): eliminate class extension and create your own properties in update-vehicle.dto.ts file.

--- backend/src/vehicles/dto/update-vehicle.dto.ts
+++ backend/src/vehicles/dto/update-vehicle.dto.ts
@@ -1,4 +1,44 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateVehicleDto } from './create-vehicle.dto';
+import {
+  IsDate,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Length,
+  Max,
+  Min,
+} from 'class-validator';
 
-export class UpdateVehicleDto extends PartialType(CreateVehicleDto) {}
+export class UpdateVehicleDto {
+  @IsNumber()
+  id!: number;
+
+  @IsOptional()
+  @IsString()
+  @Length(3, 20)
+  licensePlate?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(17, 17)
+  vin?: string;
+
+  @IsOptional()
+  @IsString()
+  brand?: string;
+
+  @IsOptional()
+  @IsString()
+  model?: string;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1900)
+  @Max(Number(new Date().getFullYear()))
+  productionYear?: number;
+
+  @IsDate()
+  @Min(Date.parse('1900, 1, 1'))
+  @Max(new Date().getFullYear())
+  createdAt!: Date;
+}
