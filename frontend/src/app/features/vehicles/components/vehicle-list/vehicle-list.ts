f122b6792b8b3dc49f92227b9a95fd49f5e13be1
zstorok75 <zstorok75@gmail.com>
2026-07-08 21:45:01 +0200

    feat(frontend): create a table for vehicles list with filtering and sorting options and create the logic

--- frontend/src/app/features/vehicles/components/vehicle-list/vehicle-list.ts
+++ frontend/src/app/features/vehicles/components/vehicle-list/vehicle-list.ts
@@ -1,15 +1,57 @@
-import { Component, inject, signal } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  effect,
+  inject,
+  OnInit,
+  signal,
+  viewChild,
+  ViewChild,
+  WritableSignal,
+} from '@angular/core';
 import { VehicleService } from '../../services/vehicle.service';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { UpdateVehicle } from '../../models/updateVehicle.interface';
 import { Vehicle } from '../../models/vehicle.interface';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-vehicle-list',
-  imports: [],
+  imports: [MatTableModule, MatFormFieldModule, MatInputModule, MatSortModule],
   templateUrl: './vehicle-list.html',
   styleUrl: './vehicle-list.scss',
 })
-export class VehicleList {
+export class VehicleList implements OnInit {
   private vehicleService = inject(VehicleService);
+  listData = new MatTableDataSource<UpdateVehicle>();
+  private sort = viewChild(MatSort);
+  displayedColumns: string[] = ['index', 'brand', 'model', 'vin', 'licensePlate', 'productionYear'];
 
-  public vehicles = signal<Vehicle[]>([]);
+  constructor() {
+    effect(() => {
+      this.listData.data = this.vehicleService.vehicleList();
+    });
+    effect(() => {
+      // A viewChild a v17-től signalt ad vissza
+      const sortInstance = this.sort();
+      if (sortInstance) {
+        this.listData.sort = sortInstance;
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.vehicleService.getAllVehicles();
+  }
+
+  selectVehicle(vehicle: Vehicle) {
+    console.log(`Kattintottam a ${vehicle.id} járműre`);
+  }
+
+  applyFilter(event: Event) {
+    const filteredList = (event.target as HTMLInputElement).value;
+    this.listData.filter = filteredList.trim().toLowerCase();
+  }
 }
