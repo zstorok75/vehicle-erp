259e05ab4bcbfbb37d85bf7220323d2bb51ebb52
zstorok75 <zstorok75@gmail.com>
2026-07-08 21:49:57 +0200

    feat(frontend): add VehicleList & VehicleForm components to the routes list

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { VehicleForm } from './features/vehicles/components/vehicle-form/vehicle
 import { VehicleList } from './features/vehicles/components/vehicle-list/vehicle-list';
 
 export const routes: Routes = [
-  // { path: 'vehicle-form', component: VehicleForm },
-  // { path: 'vehicle-list', component: VehicleList },
+  { path: 'vehicle-form', component: VehicleForm, title: 'Vehicle form' },
+  { path: 'vehicle-list', component: VehicleList, title: 'Vehicle list' },
   { path: '**', component: VehicleForm },
 ];
