5f2a6255bf2e25ccde37c407e8e87a1bd1e610f9
zstorok75 <zstorok75@gmail.com>
2026-07-15 19:30:54 +0200

    feat(frontend): Create vehicle pages own routes

--- frontend/src/app/features/vehicles/vehicle.routes.ts
+++ frontend/src/app/features/vehicles/vehicle.routes.ts
@@ -2,20 +2,33 @@ import { Routes } from '@angular/router';
 export const VEHICLE_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./components/vehicle-list/vehicle-list').then((m) => m.VehicleList),
     title: 'Járművek - AutoManager',
-  },
-  {
-    path: 'new',
     loadComponent: () =>
-      import('./components/vehicle-form/vehicle-form').then((m) => m.VehicleForm),
-    title: 'Új jármű felvétele',
-  },
-  {
-    path: ':id',
-    loadComponent: () =>
-      import('./components/vehicle-form/vehicle-form').then((m) => m.VehicleForm),
-    title: 'Jármű részletei',
+      import('./components/vehicle-shell/vehicle-shell').then((m) => m.VehicleShell),
+    children: [
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full',
+      },
+      {
+        path: 'list',
+        loadComponent: () =>
+          import('./components/vehicle-list/vehicle-list').then((m) => m.VehicleList),
+        title: 'Járművek',
+      },
+      {
+        path: 'new',
+        loadComponent: () =>
+          import('./components/vehicle-form/vehicle-form').then((m) => m.VehicleForm),
+        title: 'Új jármű rögzítése',
+      },
+      {
+        path: ':id',
+        loadComponent: () =>
+          import('./components/vehicle-form/vehicle-form').then((m) => m.VehicleForm),
+        title: 'Jármű részletei',
+      },
+    ],
   },
 ];
