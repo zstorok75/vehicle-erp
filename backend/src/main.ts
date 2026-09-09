0b0c56dbcbc525687fa9825a40fe0e33af854c53
zstorok75 <zstorok75@gmail.com>
2026-09-09 15:43:36 +0200

    feat(backend): add Transfer Interceptor to main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,7 @@ async function bootstrap() {
 
   // Globális kivételszűrő aktiválása
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   // SWAGGER BEÁLLÍTÁSA
   const config = new DocumentBuilder()
