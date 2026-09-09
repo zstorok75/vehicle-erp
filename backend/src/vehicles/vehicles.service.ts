09f7267a7da65bd6caa7d61d2ec8a4a85a56a952
zstorok75 <zstorok75@gmail.com>
2026-09-09 15:54:09 +0200

    feature(backend): add additional procedures to service methods to use global exceptions

--- backend/src/vehicles/vehicles.service.ts
+++ backend/src/vehicles/vehicles.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Vehicle } from './entities/vehicle.entity';
 import { Repository } from 'typeorm';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class VehiclesService {
@@ -14,9 +14,17 @@ export class VehiclesService {
 
   // Új jármű mentése az adatbázisba
   async create(createVehicleDto: CreateVehicleDto): Promise<Vehicle> {
-    // A DTO alapján létrehozunk egy új Vehicle példányt
-    const newVehicle = this.vehicleRepository.create(createVehicleDto);
-    return this.vehicleRepository.save(newVehicle);
+    // Ellenőrizzük alvázszám alapján, hogy létezik e, ha igen hibát dobunk?
+    if (await this.existsByVin(createVehicleDto.vin)) {
+      throw new HttpException(
+        'Ez az alvázszám már szerepel a rendszerben!',
+        HttpStatus.CONFLICT,
+      );
+    } else {
+      // A DTO alapján létrehozunk egy új Vehicle példányt
+      const newVehicle = this.vehicleRepository.create(createVehicleDto);
+      return this.vehicleRepository.save(newVehicle);
+    }
   }
 
   // Összes jármű lekérdezése
@@ -25,23 +33,75 @@ export class VehiclesService {
   }
 
   // Lekérni egy autót az "id" alapján
-  async findOne(id: number): Promise<Vehicle | null> {
-    const vehicle: Vehicle | null = await this.vehicleRepository.findOne({
-      where: { id: id },
-    });
-    return vehicle;
-  }
-
-  async update(id: number, vehicle: UpdateVehicleDto): Promise<Vehicle | null> {
-    if (await this.existsBy(id)) {
-      await this.vehicleRepository.update(id, vehicle);
-      return this.findOne(id);
+  async findOneById(
+    id: number,
+    isNullReturnAccept: boolean,
+  ): Promise<Vehicle | null> {
+    if (isNullReturnAccept || (await this.existsById(id))) {
+      return this.vehicleRepository.findOne({ where: { id: id } });
     } else {
-      return null;
+      throw new HttpException(
+        'A jármű nem található a rendszerben!',
+        HttpStatus.NOT_FOUND,
+      );
     }
   }
 
-  async existsBy(id: number): Promise<boolean> {
+  async findOneByVin(
+    vin: string,
+    isNullReturnAccept: boolean,
+  ): Promise<Vehicle | null> {
+    if (isNullReturnAccept || (await this.existsByVin(vin))) {
+      return await this.vehicleRepository.findOneBy({ vin: vin });
+    } else {
+      throw new HttpException(
+        'A keresett alvázszám nem található',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+  }
+
+  // TODO
+  // A megfelelő jogosultság meglétekor elfogadjuk az alvázszám módosítását
+  async update(id: number, vehicle: UpdateVehicleDto): Promise<Vehicle | null> {
+    // Ellenőrizzük, hogy a megadott id és a vehicle.id azonos e.
+    //  ha nem Exception
+    if (id === vehicle.id) {
+      const vehicleById: Vehicle | null = await this.findOneById(id, true);
+      const vehicleByVin: Vehicle | null = await this.findOneByVin(
+        vehicle.vin,
+        true,
+      );
+      // összehasonlítani a kettőt id és vin alapján
+      if (
+        vehicleById !== null &&
+        vehicleByVin !== null &&
+        vehicleById.vin === vehicleByVin.vin &&
+        vehicleById.id === vehicleByVin.id
+      ) {
+        // ha a 2 vin és az id is azonos mentjük
+        await this.vehicleRepository.update(id, vehicle);
+        return this.findOneById(id, false);
+      } else {
+        throw new HttpException(
+          'A megadott ID és a jármű ID nem azonos',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+    } else {
+      throw new HttpException(
+        'A megadott adatok nem konzisztensek!',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async existsById(id: number): Promise<boolean> {
     return this.vehicleRepository.existsBy({ id: id });
+  }
+
+  async existsByVin(vin: string): Promise<boolean> {
+    // Lekérjük az alvázszám szerint egy járművet
+    return this.vehicleRepository.existsBy({ vin: vin });
   }
 }
